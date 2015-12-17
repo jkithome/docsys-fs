5a29ad21a46c9eb0183dd706538eaf6ca9d6901b
Jeremy Kithome <jeremy.kithome@andela.com>
2015-12-17 12:01:05 +0300

    Add documents model

---
+++ server/models/documents.js
@@ -0,0 +1,31 @@
+// Get an instance of mongoose and mongoose.Schema
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// Set up a mongoose model and pass it using module.exports
+module.exports = mongoose.model('Document', new Schema({
+  title: {
+    type: String,
+    unique: true,
+    required: true
+  },
+
+  genre: {
+    type: String,
+    required: true
+  },
+
+  content: String,
+
+  access: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Role'
+  }],
+
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
+}, {
+  timestamps: true
+}));
