a74738eaafcb7708f3b8c361d4fd8fe8de6013c4
Jeremy Kithome <jeremy.kithome@andela.com>
2015-12-17 15:04:14 +0300

    Add users routes

---
+++ server/routes/users.js
@@ -0,0 +1,24 @@
+(function() {
+  'use strict';
+  var Users = require('../controllers/users');
+  var Auth = require('../services/auth');
+  module.exports = function(app) {
+    // Login in user
+    app.route('/api/users/login')
+      .post(Users.login);
+    // Create user
+    app.route('/api/users/create')
+      .post(Users.create);
+
+    // Get all users
+    app.route('/api/users')
+      .get(Auth.authenticate, Users.all);
+
+    // Find, update or delete a particular user
+    app.route('/api/users/:id')
+      .get(Auth.authenticate, Users.find)
+      .put(Auth.authenticate, Users.update)
+      .delete(Auth.authenticate, Users.delete);
+
+  };
+})();
