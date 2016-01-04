caa63024f62486139561cc210ca02336689063ab
Jeremy Kithome <jeremy.kithome@andela.com>
2016-01-04 09:48:04 +0300

    Add spec file for user tests

---
+++ test/spec/user_spec.js
@@ -0,0 +1,195 @@
+(function() {
+  'use strict';
+  var request = require('superagent');
+  var userId, token;
+  var userHelper = require('../helpers/usersHelper');
+
+  describe('User spec', function() {
+    beforeEach(function(done) {
+      userHelper.seedUsers(done);
+    });
+
+    describe('Tests for Users', function() {
+      it('should create a new user', function(done) {
+        request
+          .post('http://localhost:8080/api/users/create', {
+            username: 'Tester',
+            firstname: 'Tests',
+            lastname: 'Passed',
+            email: 'testing@api.com',
+            password: 'password',
+            role: 'user'
+          })
+          .accept('application/json')
+          .end(function(err, res) {
+            expect(res.status).toEqual(200);
+            expect(res.body.message).toBe('User created successfully.');
+            done();
+          });
+      });
+
+      it('should not create a user with duplicate username', function(done) {
+        request
+          .post('http://localhost:8080/api/users/create', {
+            username: 'Tester',
+            firstname: 'Tests',
+            lastname: 'Passed',
+            email: 'testingmail@api.com',
+            password: 'password',
+            role: 'user'
+          })
+          .accept('application/json')
+          .end(function(err, res) {
+            expect(res.body.code).toEqual(11000);
+            expect(res.body.errmsg).toBe('E11000 duplicate key error index: demoDb.users.$username_1 dup key: { : \"Tester\" }');
+            done();
+          });
+      });
+
+      it('Username is required to create user', function(done) {
+        request
+          .post('http://localhost:8080/api/users/create', {
+            firstname: 'first',
+            lastname: 'last',
+            email: 'email@email.com',
+            password: 'password',
+            role: 'user'
+          })
+          .accept('application/json')
+          .end(function(err, res) {
+            expect(res.status).toEqual(200);
+            expect(res.body.message).toBe('User validation failed');
+            expect(res.body.name).toBe('ValidationError');
+            done();
+          });
+      });
+
+      it('Email is required to create user', function(done) {
+        request
+          .post('http://localhost:8080/api/users/create', {
+            username: 'username',
+            firstname: 'first',
+            lastname: 'last',
+            password: 'password',
+            role: 'user'
+          })
+          .accept('application/json')
+          .end(function(err, res) {
+            expect(res.status).toEqual(200);
+            expect(res.body.message).toBe('User validation failed');
+            expect(res.body.name).toBe('ValidationError');
+            done();
+          });
+      });
+
+      it('should not create a user with duplicate email', function(done) {
+        request
+          .post('http://localhost:8080/api/users/create', {
+            username: 'Tester2',
+            firstname: 'Tests',
+            lastname: 'Passed',
+            email: 'testing@api.com',
+            password: 'password',
+            role: 'user'
+          })
+          .accept('application/json')
+          .end(function(err, res) {
+            expect(res.body.code).toEqual(11000);
+            expect(res.body.errmsg).toBe('E11000 duplicate key error index: demoDb.users.$email_1 dup key: { : "testing@api.com" }');
+            done();
+          });
+      });
+
+      it('Should return that the user successfully logged in', function(done) {
+        request
+          .post('http://localhost:8080/api/users/login', {
+            username: 'Tester',
+            password: 'password'
+          })
+          .accept('application/json')
+          .end(function(err, res) {
+            userId = res.body.user._id;
+            token = res.body.token;
+            expect(res.status).toEqual(200);
+            expect(res.body.message).toBe('User successfully logged in.');
+            expect(res.body.token).toBeDefined();
+            done();
+          });
+      });
+
+      it('Should return all the users', function(done) {
+        request
+          .get('http://localhost:8080/api/users')
+          .set('x-access-token', token)
+          .accept('application/json')
+          .end(function(err, res) {
+            expect(res.status).toEqual(200);
+            expect(res.body).toBeDefined();
+            expect(res.body.length).toBeGreaterThan(0);
+            expect(res.body.length).toBe(4);
+            expect(Object.prototype.toString.call(res.body[0])).toBe("[object Object]");
+            expect(res.body instanceof Array).toBe(true);
+            done();
+          });
+      });
+
+      it('Should return the specified user', function(done) {
+        request
+          .get('http://localhost:8080/api/users/' + userId)
+          .set('x-access-token', token)
+          .accept('application/json')
+          .end(function(err, res) {
+            expect(res.status).toEqual(200);
+            expect(res.body.password).toBeDefined();
+            expect(res.body.email).toBeDefined();
+            expect(res.body.username).toBeDefined();
+            expect(res.body.name instanceof Object).toBe(true);
+            done();
+          });
+      });
+
+      it('Should update the specified user', function(done) {
+        request
+          .put('http://localhost:8080/api/users/' + userId)
+          .set('x-access-token', token)
+          .send({
+            email: 'testing2@api.com'
+          })
+          .end(function(err, res) {
+            expect(res.status).toEqual(200);
+            expect(res.body.message).toBe('User updated succesfully.');
+            done();
+          });
+      });
+
+      it('Should delete the specified user', function(done) {
+        request
+          .delete('http://localhost:8080/api/users/' + userId)
+          .set('x-access-token', token)
+          .end(function(err, res) {
+            expect(res.status).toEqual(200);
+            expect(res.body.message).toBe('User deleted successfully.');
+            done();
+          });
+      });
+
+      it('should confirm that the user was deleted', function(done) {
+        request
+          .get('http://localhost:8080/api/users')
+          .set('x-access-token', token)
+          .accept('application/json')
+          .end(function(err, res) {
+            expect(res.status).toEqual(200);
+            expect(res.body).toBeDefined();
+            expect(res.body.length).toBeGreaterThan(0);
+            expect(res.body.length).toBe(3);
+            expect(Object.prototype.toString.call(res.body[0])).toBe("[object Object]");
+            expect(res.body instanceof Array).toBe(true);
+            done();
+          });
+      });
+
+    });
+  });
+
+})();
