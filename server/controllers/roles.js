fda16405091f0ab6e3634d9871db3c3ba82600ef
Jeremy Kithome <jeremy.kithome@andela.com>
2016-01-05 09:50:31 +0300

    Add find,update and delete methods for roles

--- server/controllers/roles.js
+++ server/controllers/roles.js
@@ -2,19 +2,10 @@
   'use strict';
   var Role = require('../models/roles');
   module.exports = {
-    all: function(req, res) {
-      Role.find({}, function(err, roles) {
-        if (err) {
-          res.send(err);
-        } else {
-          res.json(roles);
-        }
-      });
-    },
-
     create: function(req, res) {
       var role = new Role();
       role.title = req.body.title;
+      role.description = req.body.description;
 
       role.save(function(err) {
         if (err) {
@@ -26,5 +17,68 @@
         }
       });
     },
+
+    all: function(req, res) {
+      Role.find({}, function(err, roles) {
+        if (err) {
+          res.send(err);
+        } else {
+          res.json(roles);
+        }
+      });
+    },
+
+    find: function(req, res) {
+      Role.findById(req.params.id, function(err, role) {
+        if (err) {
+          res.json({
+            message: 'Error fetching role.'
+          });
+        } else {
+          res.json(role);
+        }
+      });
+    },
+
+    update: function(req, res) {
+      Role.findById(req.params.id, function(err, role) {
+        if (err) {
+          res.send(err);
+        } else {
+          if (req.body.description) {
+            role.description = req.body.description;
+          }
+          role.save(function(err) {
+            if (err) {
+              res.send(err);
+            } else {
+              res.json({
+                message: 'Role updated succesfully.'
+              });
+            }
+          });
+        }
+      });
+    },
+
+    delete: function(req, res) {
+      Role.findById(req.params.id, function(err, role) {
+        if (err) {
+          res.send(err);
+        } else {
+          role.remove({
+            _id: req.params.id
+          }, function(err) {
+            if (err) {
+              res.send(err);
+            } else {
+              res.json({
+                message: 'Role deleted successfully.'
+              });
+            }
+          });
+        }
+      });
+    }
   };
 })();
