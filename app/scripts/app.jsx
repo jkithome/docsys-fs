880f2ad10fc68a74531f76a1f8000622f61687c8
Jeremy Kithome <jeremy.kithome@andela.com>
2016-01-26 22:33:48 +0300

    [Finishes #112221905] Add login Page route

--- app/scripts/app.jsx
+++ app/scripts/app.jsx
@@ -7,13 +7,16 @@
     Router = ReactRouter.Router,
     Route = ReactRouter.Route,
     LandingPage = require('./components/LandingPage/header.jsx'),
-    SignUpPage = require('./components/SignUpPage/SignUpPage.jsx');
+    SignUpPage = require('./components/SignUpPage/SignUpPage.jsx'),
+    LoginPage = require('./components/LoginPage/LoginPage.jsx');
 
   ReactDOM.render((
     <Router>
       <Route path="/landingpage" component={LandingPage} >
       </Route>
       <Route path="/join" component={SignUpPage} >
+      </Route>
+      <Route path="/login" component={LoginPage} >
       </Route>
     </Router>
     ), document.getElementById('ui-view'));
