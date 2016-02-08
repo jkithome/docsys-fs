e63fba7d64c0dc9beb03a049cccc273fb4b85f62
Jeremy Kithome <jeremy.kithome@andela.com>
2016-02-08 11:44:25 +0300

    [Finishes #112886977] Create toasts for duplicate email or username

--- app/scripts/components/SignupPage/SignupPage.jsx
+++ app/scripts/components/SignupPage/SignupPage.jsx
@@ -46,9 +46,13 @@
 
       handleSignup: function() {
         var data = UserStore.getData();
-        if (data.error) {
-          window.Materialize.toast(data.error.message, 2000, 'error-toast');
-          this.setState({result: data.error.message});
+        if (data.code) {
+          console.log(data.errmsg);
+          if(data.errmsg.indexOf(this.state.username) !== -1) {
+            window.Materialize.toast('Username is already taken', 2000, 'error-toast');
+          } else if(data.errmsg.indexOf(this.state.email) !== -1) {
+            window.Materialize.toast('Email is already taken', 2000, 'error-toast');
+          }
         } else {
           this.setState({result: 'Success!'});
           window.Materialize.toast(data.message, 2000, 'success-toast');
