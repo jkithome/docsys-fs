66a9319da1d1a60660ea369550cd1842df00376f
Jeremy Kithome <jeremy.kithome@andela.com>
2016-02-12 16:06:39 +0300

    [#112221929] Add document constants

--- app/scripts/components/Dashboard/DocList.jsx
+++ app/scripts/components/Dashboard/DocList.jsx
@@ -5,13 +5,13 @@
       render: function() {
         var renderDoc = function(document) {
           return (
-            <div class="col s12 m12 l4" style="top: 0px;">
-              <div class="card">
-                <div class="card-image waves-effect waves-block waves-light">
-                  <a href="https://adbeus.com/montreal/cabine-m-aeroport-international-de-montreal-de/">
-                    <img width="320" height="240" src="http://lorempixel.com/600/337/nature/" class="responsive-img wp-post-image" alt={document.tile} title={document.tile}> </a>
+            <div className="col s12 m12 l4" style="top: 0px;">
+              <div className="card">
+                <div className="card-image waves-effect waves-block waves-light">
+                  <a href={'/docs/' + document.id}><p>{document.title}">
+                    <img width="320" height="240" src="http://lorempixel.com/600/337/nature/" className="responsive-img wp-post-image" alt={document.tile} title={document.tile}> </a>
                 </div>
-                <div class="card-content">
+                <div className="card-content">
                   <a href={'/docs/' + document.id}><p>{document.title}</p></a>
                   <p>{'document.name.first' + ' ' + 'document.name.last'}</p>
                   <p>Genre</p>
--- app/scripts/components/Dashboard/header.jsx
+++ app/scripts/components/Dashboard/header.jsx
@@ -7,16 +7,16 @@
         return(
           <div>
             <nav>
-              <div class="nav-wrapper">
-                <a href="#" class="brand-logo"><img class="logo-img" src="logo.gif"></a>
-                <ul class="right hide-on-med-and-down">
-                  <li><a href="/dashboard"><i class="material-icons tooltipped" data-position="bottom" data-delay="50" data-tooltip="Dashboard">dashboard</i></a></li>
-                  <li><a href="/create"><i class="material-icons tooltipped" data-position="bottom" data-delay="50" data-tooltip="Create Document">note_add</i></a></li>
-                  <li><a href="/search"><i class="material-icons tooltipped" data-position="bottom" data-delay="50" data-tooltip="Search">search</i></a></li>
-                  <li><a href="/roles"><i class="material-icons tooltipped" data-position="bottom" data-delay="50" data-tooltip="Roles">accessibility</i></a></li>
-                  <li><a href="/about"><i class="material-icons tooltipped" data-position="bottom" data-delay="50" data-tooltip="About Us">info_outline</i></a></li>
-                  <li><a href="/profile"><i class="material-icons tooltipped" data-position="bottom" data-delay="50" data-tooltip="Profile">account_circle</i></a></li>
-                  <li><a href="mobile.html"><i class="material-icons ">more_vert</i></a></li>
+              <div className="nav-wrapper">
+                <a href="#" className="brand-logo"><img className="logo-img" src="logo.gif"></a>
+                <ul className="right hide-on-med-and-down">
+                  <li><a href="/dashboard"><i className="material-icons tooltipped" data-position="bottom" data-delay="50" data-tooltip="Dashboard">dashboard</i></a></li>
+                  <li><a href="/create"><i className="material-icons tooltipped" data-position="bottom" data-delay="50" data-tooltip="Create Document">note_add</i></a></li>
+                  <li><a href="/search"><i className="material-icons tooltipped" data-position="bottom" data-delay="50" data-tooltip="Search">search</i></a></li>
+                  <li><a href="/roles"><i className="material-icons tooltipped" data-position="bottom" data-delay="50" data-tooltip="Roles">accessibility</i></a></li>
+                  <li><a href="/about"><i className="material-icons tooltipped" data-position="bottom" data-delay="50" data-tooltip="About Us">info_outline</i></a></li>
+                  <li><a href="/profile"><i className="material-icons tooltipped" data-position="bottom" data-delay="50" data-tooltip="Profile">account_circle</i></a></li>
+                  <li><a href="mobile.html"><i className="material-icons ">more_vert</i></a></li>
                 </ul>
               </div>
             </nav>
