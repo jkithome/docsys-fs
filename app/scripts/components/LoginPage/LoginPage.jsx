48eff7eb48bdd49cb86ac837d50cae7439cb6381
Jeremy Kithome <jeremy.kithome@andela.com>
2016-02-13 08:12:57 +0300

    [#112221929] Add user detail storage to local storage

--- app/scripts/components/LoginPage/LoginPage.jsx
+++ app/scripts/components/LoginPage/LoginPage.jsx
@@ -26,7 +26,8 @@
 
       handleLogin: function() {
         var data = UserStore.getData();
-        localStorage.setItem('x-access-token', data.token)
+        //localStorage.setItem('x-access-token', data.token);
+        localStorage.setItem('user', JSON.stringify(data.user));
         console.log(data);
         if(data.error) {
           if(typeof data.error === 'string') {
