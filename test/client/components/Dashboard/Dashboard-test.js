e01b0d7708bb0dd16ecdd99e0337dea0b3f6a9d6
Jeremy Kithome <jeremy.kithome@andela.com>
2016-02-25 21:44:21 +0300

    [#114381847] Add initial front end tests

---
+++ test/client/components/Dashboard/Dashboard-test.js
@@ -0,0 +1,22 @@
+(function() {
+  'use strict';
+
+  var expect = require('chai').expect;
+  var enzyme = require('enzyme');
+  var DashboardPath = '../../../../app/scripts/components/Dashboard/index.jsx';
+  var React = require('react');
+  var Dashboard = require(DashboardPath);
+
+  describe('Dashboard', function() {
+    it('renders the Dashboard component', function() {
+      // Render Dashboard page in the document
+      var dashboard = enzyme.shallow(<Dashboard />);
+
+      // Test the orgs state
+      expect(dashboard.state().users).to.eql(null);
+      expect(dashboard.state().documents).to.eql(null);
+      expect(dashboard.state().userDocuments).to.eql(null);
+      expect(dashboard.state().byUserDocuments).to.eql(null);
+    });
+  });
+})();
---
+++ test/testdom.js
@@ -0,0 +1,27 @@
+/* setup.js */
+
+var jsdom = require('jsdom');
+var localStorage = require('localStorage');
+var doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+
+var exposedProperties = ['window', 'navigator', 'document'];
+
+global.document = doc;
+global.window = document.defaultView;
+global.window.$ = require('jquery');
+global.window.localStorage = localStorage;
+global.localStorage = localStorage;
+global.window.$.fn.sideNav = function() {
+  return;
+};
+
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property);
+    global[property] = document.defaultView[property];
+  }
+});
+
+global.navigator = {
+  userAgent: 'node.js'
+};
