5a445581af521227d044beb9629c0aa5f71e44d7
Jeremy Kithome <jeremy.kithome@andela.com>
2016-02-25 23:32:26 +0300

    [#114381847] Add tests for landing page component

---
+++ test/client/components/LandingPage/LandingPage-test.js
@@ -0,0 +1,24 @@
+(function() {
+  'use strict';
+
+  var expect = require('chai').expect;
+  var enzyme = require('enzyme');
+  var sinon = require('sinon');
+  var LandingPage = '../../../../app/scripts/components/LandingPage/header.jsx';
+  var React = require('react');
+  var Landing = require(LandingPage);
+
+  describe('Landing component', function() {
+  it('renders the correct content', function() {
+    expect(enzyme.shallow(<Landing />).text()).to.have.string('The simplest way to manage your documents.');
+    expect(enzyme.shallow(<Landing />).text()).to.have.string('GET STARTED');
+  });
+
+  it('renders the correct component', function() {
+    expect(enzyme.shallow(<Landing />).is('#landing')).to.eql(true);
+    expect(enzyme.mount(<Landing />).find('.container').length).to.eql(1);
+    expect(enzyme.mount(<Landing />).find('.join-btn').length).to.eql(1);
+    expect(enzyme.mount(<Landing />).find('#nav-mobile').length).to.eql(1);
+  });
+});
+})();
