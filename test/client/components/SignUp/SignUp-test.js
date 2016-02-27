6579883a3571a4c1f22b168ae1ef1099e0a1e261
Jeremy Kithome <jeremy.kithome@andela.com>
2016-02-27 17:35:29 +0300

    [#114381847] Add tests for correct content and field change handler

--- test/client/components/SignUp/SignUp-test.js
+++ test/client/components/SignUp/SignUp-test.js
@@ -13,10 +13,8 @@
 
   describe('SignUp', function() {
     window.Materialize = {};
-    browserHistory = {};
     before(function() {
         window.Materialize.toast = sinon.spy();
-        browserHistory.push = sinon.spy();
     });
     it('renders the signup component', function() {
       // Render Dashboard page in the document
@@ -27,6 +25,17 @@
       expect(signup.state().lastname).to.eql(null);
       expect(signup.state().username).to.eql(null);
       expect(signup.state().role).to.have.string('user');
+    });
+
+    it('renders the correct component', function() {
+      expect(enzyme.mount(<SignUp />).find('.nav-wrapper').length).to.be.at.least(1);
+      expect(enzyme.mount(<SignUp />).find('.row').length).to.be.at.least(7);
+      expect(enzyme.mount(<SignUp />).find('.card-panel').length).to.be.at.least(1);
+    });
+
+    it('renders the correct content', function() {
+      expect(enzyme.shallow(<SignUp />).text()).to.have.string('Sign Up Today');
+      expect(enzyme.shallow(<SignUp />).text()).to.have.string('JOIN');
     });
 
     it('calls componentDidMount', function() {
@@ -52,22 +61,28 @@
     });
 
     it('calls the user signup change listener', function() {
+      sinon.stub(browserHistory, 'push').returns(true);
         sinon.spy(UserStore, 'getCreatedUser');
         enzyme.mount(<SignUp />); // Mount the component
         // Trigger a change in the UserStore
         UserStore.setCreatedUser({message: 'user created successfully'});
         // The getCreatedUser function should be called
+        expect(browserHistory.push.called).to.eql(true);
         expect(UserStore.getCreatedUser.called).to.eql(true);
         UserStore.getCreatedUser.restore();
+        browserHistory.push.restore();
       });
 
     it('sets the correct state if the response is valid', function() {
+      sinon.stub(browserHistory, 'push').returns(true);
         var signup = enzyme.mount(<SignUp />);
         // Trigger a change in the UserStore
         UserStore.setCreatedUser({message: 'user created successfully'});
         expect(UserStore.getCreatedUser()).to.be.an('object');
+        expect(browserHistory.push.called).to.eql(true);
         expect(signup.state().result).to.be.a('string');
         expect(signup.state().result).to.eql('Success!');
+        browserHistory.push.restore();
       });
 
       it('sets the correct state if the response has an error', function() {
@@ -80,6 +95,34 @@
         expect(UserStore.getCreatedUser()).to.be.an('object');
         expect(signup.state().result).to.be.a('string');
         expect(signup.state().result).to.eql('Failed!');
+      });
+
+      it('should correctly handle field change', function() {
+        var signup = enzyme.mount(<SignUp />);
+        var event = {
+          target: {
+            name: 'firstname',
+            value: 'katniss'
+          },
+          preventDefault: function() {}
+        };
+        var instance = signup.instance();
+        sinon.spy(instance, 'handleFieldChange');
+        instance.handleFieldChange(event);
+        expect(signup.state()[event.target.name]).to.eql(event.target.value);
+        instance.handleFieldChange.restore();
+      });
+
+      it('should correctly handle search select', function() {
+        var signup = enzyme.mount(<SignUp />);
+        var event = {
+            value: 'content',
+        };
+        var instance = signup.instance();
+        sinon.spy(instance, 'handleRoleSelect');
+        instance.handleRoleSelect(event);
+        expect(signup.state().role).to.eql(event.value);
+        instance.handleRoleSelect.restore();
       });
   });
 })();
