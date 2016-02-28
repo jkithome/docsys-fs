875694e0e3dd64bf479616db2c92c9fdaa5ac535
Jeremy Kithome <jeremy.kithome@andela.com>
2016-02-28 16:55:36 +0300

    [#114381847] Finish statistics component tests

--- test/client/components/Statistics/Statistics-test.js
+++ test/client/components/Statistics/Statistics-test.js
@@ -9,28 +9,21 @@
   var Statistics = require(StatisticsPath);
 
   describe('Statistics component', function() {
-    var users = [];
-    var documents = [];
-    var userDocuments = [];
+    var users = [{},{},{}];
+    var documents = [{},{},{}];
+    var userDocuments = [{},{},{}];
   it('renders the correct content', function() {
-    sinon.stub(users, 'length').returns(5);
-    sinon.stub(documents, 'length').returns(30);
-    sinon.stub(userDocuments, 'length').returns(2);
-    var statistics = enzyme.shallow(<Statistics />);
-    statistics.setProps({ users: [1,2,3], documents: [1,2,3,4], userDocuments: [1,2,3,4,5] });
-    expect(statistics.is('.card-stats')).to.eql(true);
+    var statistics = enzyme.mount(<Statistics users={users} documents={documents} userDocuments={userDocuments}/>);
     expect(statistics.text()).to.have.string('Total Users');
     expect(statistics.text()).to.have.string('Total Documents');
     expect(statistics.text()).to.have.string('My Documents');
-    users.length.restore();
-    documents.length.restore();
-    userDocuments.length.restore();
   });
 
   it('renders the correct component', function() {
-    expect(enzyme.mount(<Statistics />).find('.card').length).to.be.at.least(3);
-    expect(enzyme.mount(<Statistics />).find('.card-stats').length).to.eql(1);
-    expect(enzyme.mount(<Statistics />).find('row').length).to.eql(1);
+    var statistics = enzyme.mount(<Statistics users={users} documents={documents} userDocuments={userDocuments}/>);
+    expect(statistics.find('.card').length).to.be.at.least(3);
+    expect(statistics.find('.card-stats-title').length).to.eql(3);
+    expect(statistics.find('.row').length).to.eql(1);
   });
 });
 })();
