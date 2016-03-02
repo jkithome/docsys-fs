cd55b9ccbdccd20286665f17dd8aee5a6be129f7
Jeremy Kithome <jeremy.kithome@andela.com>
2016-03-02 22:03:22 +0300

    [#114858005] Remove redudant search routes

--- server/routes/documents.js
+++ server/routes/documents.js
@@ -8,22 +8,6 @@
       .post(Auth.authenticate, Documents.create)
       .get(Auth.authenticate, Documents.all);
 
-    // All documents by genre
-    app.route('/api/documents/genre')
-      .get(Auth.authenticate, Documents.allByGenre);
-
-    // All documents with a given word in the content field
-    app.route('/api/documents/search')
-      .get(Auth.authenticate, Documents.allByContent);
-
-    // All documents created on a given date
-    app.route('/api/documents/date')
-      .get(Auth.authenticate, Documents.allByDate);
-
-    // All documents accessible by a given role
-    app.route('/api/documents/role')
-      .get(Auth.authenticate, Documents.allByRole);
-
     // Route to find, update or delete a specific document
     app.route('/api/documents/:id')
       .get(Auth.authenticate, Documents.find)
