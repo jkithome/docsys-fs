6fa4e83515916c438a77768ea191cb6b148d3948
Jeremy Kithome <jeremy.kithome@andela.com>
2016-03-07 12:38:38 +0300

    [#114858005] Remove data picker initialization from search component

--- app/scripts/components/Search/Search.jsx
+++ app/scripts/components/Search/Search.jsx
@@ -25,10 +25,6 @@
 
     componentDidMount: function() {
       DocumentStore.addChangeListener(this.populateDocuments, 'search');
-      window.$('.datepicker').pickadate({
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 15 // Creates a dropdown of 15 years to control year
-      });
     },
 
     componentWillUnmount() {
