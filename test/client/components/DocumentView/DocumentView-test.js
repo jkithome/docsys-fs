3fa1aa7709b15f1ea59b629cfb4b2b95b864e019
Jeremy Kithome <jeremy.kithome@andela.com>
2016-03-07 14:10:49 +0300

    [#114858005] Change tests to reflect new content

--- test/client/components/DocumentView/DocumentView-test.js
+++ test/client/components/DocumentView/DocumentView-test.js
@@ -12,6 +12,35 @@
 
   describe('DocumentView', function() {
     window.Materialize = {};
+    var doc = {
+      "_id": "56cf4520acc801dd33939b95",
+      "updatedAt": "2016-02-25T18:17:04.000Z",
+      "createdAt": "2016-02-25T18:17:04.000Z",
+      "genre": "Fantasy",
+      "owner": {
+        "_id": "56cf451bacc801dd33939b79",
+        "updatedAt": "2016-02-28T08:28:38.818Z",
+        "createdAt": "2016-02-25T18:16:59.000Z",
+        "email": "jerry@erry.com",
+        "username": "Jemmy",
+        "role": "56cf451b09db67e133ab284d",
+        "__v": 0,
+        "loggedIn": true,
+        "token": "eyJ",
+        "name": {
+          "last": "Kithome",
+          "first": "Jeremy"
+        }
+      },
+      "content": "30 years after the defeat of the Galactic Empire, a new threat rises. The First Order attempts to rule the galaxy and only a ragtag group of Heroes can stop them, along with the help of the Resistance.",
+      "title": "Star Wars: The Force Awakens",
+      "__v": 0,
+      "access": [
+        "56cf451b09db67e133ab284d",
+        "56cf451b09db67e133ab284f",
+        "56cf451b09db67e133ab284e"
+      ]
+    };
     before(function() {
         window.Materialize.toast = sinon.spy();
     });
@@ -22,14 +51,9 @@
       expect(documentView.state().document).to.eql(null);
     });
 
-    it('renders the correct component', function() {
-      var documentView = enzyme.mount(<DocumentView />);
-      expect(documentView.find('.row').length).to.be.at.least(1);
-    });
-
     it('renders the correct content', function() {
       expect(enzyme.shallow(<DocumentView />).find('Header').length).to.be.at.least(1);
-      expect(enzyme.shallow(<DocumentView />).find('CreateDocument').length).to.be.at.least(1);
+      expect(enzyme.shallow(<DocumentView />).find('.progress').length).to.eql(1);
     });
 
     it('calls componentDidMount', function() {
