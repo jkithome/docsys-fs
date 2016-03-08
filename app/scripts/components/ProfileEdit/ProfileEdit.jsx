c26c2c6195dd57e1d63b634a25e575de07a1b776
Jeremy Kithome <jeremy.kithome@andela.com>
2016-03-08 11:41:58 +0300

    [#115225025] Fix profile edit buttons responsiveness

--- app/scripts/components/ProfileEdit/ProfileEdit.jsx
+++ app/scripts/components/ProfileEdit/ProfileEdit.jsx
@@ -206,15 +206,17 @@
                       </div>
                     </div>
                     <div className="row">
-                      <div className="col s2 offset-s4">
-                        <button id="cancel" className="btn waves-effect red accent-2 center" onClick={this.onCancel}>
-                          cancel
-                        </button>
-                      </div>
-                      <div className="col s2">
-                        <button id="submit" className="btn waves-effect teal center" onClick={this.onSubmit}>
-                          update
-                        </button>
+                      <div className="col s4 offset-s4 create-btns">
+                        <div className="col s12 m12 l6">
+                          <button id="cancel" className="btn waves-effect red accent-2 center" onClick={this.onCancel}>
+                            cancel
+                          </button>
+                        </div>
+                        <div className="col s12 m12 l6">
+                          <button id="submit" className="btn waves-effect teal" onClick={this.onSubmit}>
+                            update
+                          </button>
+                        </div>
                       </div>
                       <div className="section">
                       </div>
